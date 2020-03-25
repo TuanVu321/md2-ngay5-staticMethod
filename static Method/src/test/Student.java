a8afd6c404ea75523f699167e5d592feb57eaa26
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-03-25 14:09:04 +0700

    Add files via upload

---
+++ static Method/src/test/Student.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    public Student(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    public static void change() {
+        Student.college = "CODEGYM";
+    }
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
